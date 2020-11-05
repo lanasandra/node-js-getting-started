891df0640f5dbd9a6ccf3198c9a5a74005e5a018
lanasandra <sandra.cabessa@gmail.com>
2020-11-05 12:05:54 +0100

    response

--- server.js
+++ server.js
@@ -54,15 +54,12 @@ app.post('/api/getAccounts', (req, res) => {
 // https://still-stream-63740.herokuapp.com/api/getAccounts
 app.post("/clicked", (req, res) => {
  
-  client.query('SELECT firstName, LastName FROM salesforce.Contact')
-  
-  .then(response => {
-    console.log('***** response', response);
-    res.status(200).json(response)
-  })
-  .catch(err => {
-    res.status(500).json({ "message": err});
-
+  client.query('SELECT firstName, LastName FROM salesforce.Contact', function(err,result){
+    if(err) res.send(err)
+    res.json(result)
+    res.send({
+      response: result
+    })
   })
 })
 
