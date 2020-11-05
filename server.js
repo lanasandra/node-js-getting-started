4feb81e9fd3d8cb80321601508ba6408ab0498dc
lanasandra <sandra.cabessa@gmail.com>
2020-11-05 08:57:31 +0100

    other test response

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/api/getAccounts', (req, res) => {
 app.post('/api/getAccounts2', (req, res) => {
   client.query('SELECT FirstName FROM salesforce.Contact WHERE Email=jane_gray@uoa.edu').then(response => {
       console.log('***** response', response);
-      res.status(200).json({ "message": "Il y'a " + response.rows.firstName + " Contacts"});
+      res.status(200).json({ "message": "Il y'a " + response + " Contacts"});
   }).catch(err => {
       res.status(500).json({ "message": err});
 
