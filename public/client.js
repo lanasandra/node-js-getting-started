11a9600aafd62454d69a2cc807785f21680b05e2
lanasandra <sandra.cabessa@gmail.com>
2020-11-07 16:57:17 +0100

    test

--- public/client.js
+++ public/client.js
@@ -68,11 +68,11 @@ loginButton.addEventListener('click', function(e){
 
   let request = new XMLHttpRequest();
   request.open('POST', 'api/login', true);
-  //request.setRequestHeader('Content-Type', 'application/json');
+  request.setRequestHeader('Content-Type', 'application/json');
   request.responseType = 'json';
   var data = JSON.stringify({
-    "username": emailInput.value,
-    "password": passwordInput.value
+    "username": String(emailInput.value),
+    "password": String(passwordInput.value)
   })
   console.log(data);
   request.send(data);
